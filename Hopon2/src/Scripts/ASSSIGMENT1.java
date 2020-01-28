1f37c7b48b0b2a349138f0ab0f6c50d1f9429ff5
Admin <Admin@192.168.1.3>
2020-01-28 15:38:51 +0530

    Ninth comitt

---
+++ Hopon2/src/Scripts/ASSSIGMENT1.java
@@ -0,0 +1,42 @@
+package Scripts;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import Generic.BaseTest;
+import Generic.Lib;
+import Pompages.Loginpage1;
+
+public class ASSSIGMENT1 extends BaseTest{
+	
+	@Test
+	public void testValidLogin2() throws InterruptedException {
+		
+		Loginpage1 lp=new Loginpage1(driver);
+		String username = Lib.getCellValue(EXCEL_PATH,"ValidLogin",1,0);
+		lp.setUsername(username);
+		 Thread.sleep(3000);
+		 
+		 String st2 = Lib.getCellValue(EXCEL_PATH,"ValidLogin",1,1);
+		/* double st1 = Lib.getIntvalue(EXCEL_PATH,"ValidLogin",1,1);
+		 int a=(int) st1;
+		 String st2= Integer.toString(a);*/
+		 lp.setPassWord1(st2);
+		 Thread.sleep(3000);
+		  
+	     lp.clickLogin();
+		
+	     WebDriverWait wait=new WebDriverWait(driver,30);
+		 wait.until(ExpectedConditions.titleIs("actiTIME - Enter Time-Track"));
+		
+		
+		SoftAssert s= new SoftAssert();
+	    s.assertEquals(driver.getTitle(),"actiTIME - Enter Time-Track");
+	    s.assertAll();
+		
+	    }
+
+
+}
